5981c69bacb3258ae8dc99b59f39bd2dccc34c55
Erich Polixa <epolixa@gmail.com>
2014-11-10 13:16:24 -0500

    stuff

--- js/onload.js
+++ js/onload.js
@@ -2,22 +2,34 @@
 
 $(document).ready(function() {
 
-	/* Load header HTML */
-	$.ajax({
-	  	url: "linked/header.html",
-	  	dataType: "html",
-	  	success: 	function(data) {
-						$('body').html(data);
-					}
+	/* Animate orbit list */
+	/* List orbits at beginning and when mouse is inactive for 5 seconds */
+	/* List pauses orbit when mouse moves */
+	orbit_speed = 0;
+	$('.primary_content .orbit_list').animate({
+		/* etc */
+	}, {
+		step: function(now, fx) {
+			var orbit_speed = 1;
+			if (orbit_speed > 1) orbit_speed -= 1;
+			$('.primary_content h2').click(function() {
+				orbit_speed += 8;
+				if (orbit_speed >= 10) orbit_speed = 10;
+			});
+
+
+
+			$('.primary_content h2').after('<div>' + orbit_speed + '</div>');
+		}
 	});
 
-	/* Load footer HTML */
-	$.ajax({
-	  	url: "linked/footer.html",
-	  	dataType: "html",
-	  	success: 	function(data) {
-						$('body').html(data);
-					}
+	/* Resume area */
+	$('.primary_content .orbit_list #li_resume').click(function() {
+		$('.resume_area').animate({
+			width: 'toggle'
+		}, 200, function() {
+			/* etc */
+		});
 	});
 
 });
